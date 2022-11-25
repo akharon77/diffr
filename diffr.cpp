e4ff595e9a8f4ec5e786cfece33a86e846991908
akharon77 <abdullin.ta@phystech.edu>
2022-11-25 05:13:19 +0300

    ref: dsl, parser

--- diffr.cpp
+++ diffr.cpp
@@ -11,6 +11,8 @@
 #include "iostr.h"
 #include "stack.h"
 #include "colors.h"
+#include "parser.h"
+#include "dsl.h"
 
 const Option EXEC_OPTIONS[] = 
     {
@@ -57,8 +59,8 @@ void DiffrDump(Diffr *diffr)
 
     static int32_t cnt = 0;
 
-    char filename_txt[256] = "",
-         filename_svg[256] = "";
+    char filename_txt[512] = "",
+         filename_svg[512] = "";
 
     sprintf(filename_txt, "dump/difft_%s_%d.txt", diffr->filename, cnt);
     sprintf(filename_svg, "dump/diffr_%s_%d.svg", diffr->filename, cnt++);
@@ -72,7 +74,7 @@ void DiffrDump(Diffr *diffr)
 
     close(fd);
 
-    char cmd[256] = "";
+    char cmd[512] = "";
     sprintf(cmd, "dot %s -o %s -Tsvg", filename_txt, filename_svg);
     system(cmd);
 }
@@ -183,8 +185,6 @@ Node* Differentiate(Node *node)
     }
 }
 
-#undef CURR
-
 Node *CreateNode(int32_t type, NodeValue val, Node *left, Node *right)
 {
     Node *node = NodeNew();
@@ -193,203 +193,6 @@ Node *CreateNode(int32_t type, NodeValue val, Node *left, Node *right)
 
     return node;
 }
-
-const char *GetGeneral(const char *str, Node *value)
-{
-    str = GetExpression(str, value);
-
-    assert(*str == '\0');
-    ++str;
-
-    return str;
-}
-
-const char *GetExpression(const char *str, Node *value)
-{
-    Node *top_node = NodeNew();
-    str = GetProduct(str, top_node);
-
-    while (*str == '+' || *str == '-')
-    {
-        char op = *str;
-        ++str;
-
-        Node *buf_val = NodeNew();
-        str = GetProduct(str, buf_val);
-
-        switch (op)
-        {
-            case '+':
-                top_node = ADD(top_node, buf_val);
-                break;
-            case '-':
-                top_node = SUB(top_node, buf_val);
-                break;
-            default:
-                assert(0 && "Syntax error\n");
-        }
-    }
-
-    *value = *top_node;
-    free(top_node);
-
-    return str;
-}
-
-const char *GetProduct(const char *str, Node *value)
-{
-    Node *top_node = NodeNew();
-    str = GetPower(str, top_node);
-
-    while (*str == '*' || *str == '/')
-    {
-        char op = *str;
-        ++str;
-
-        Node *buf_val = NodeNew();
-        str = GetPower(str, buf_val);
-
-        switch (op)
-        {
-            case '*':
-                top_node = MUL(top_node, buf_val);
-                break;
-            case '/':
-                top_node = DIV(top_node, buf_val);
-                break;
-            default:
-                assert(0 && "Syntax error\n");
-        }
-    }
-
-    *value = *top_node;
-    free(top_node);
-
-    return str;
-}
-
-const char *GetPower(const char *str, Node *value)
-{
-    Node *top_node = NodeNew();
-    str = GetPrimary(str, top_node);
-
-    while (*str == '^')
-    {
-        Node *buf_val = NodeNew();
-
-        ++str;
-        str = GetPrimary(str, buf_val);
-
-        top_node = EXP(top_node, buf_val);
-    }
-
-    *value = *top_node;
-    free(top_node);
-
-    return str;
-}
-
-const char *GetPrimary(const char *str, Node *value)
-{
-    if (*str == '(')
-    {
-        ++str;
-        str = GetExpression(str, value);
-
-        assert(*str == ')');
-        ++str;
-    }
-    else if (isdigit(*str))
-    {
-        str = GetNumber(str, value);
-    }
-    else if (*str == 'x')
-    {
-        str = GetVariable(str, value);
-    }
-    else
-    {
-        str = GetFunction(str, value);
-    }
-
-    return str;
-}
-
-const char *GetNumber(const char *str, Node *value)
-{
-    int32_t res = 0;
-    const char *str_old = str;
-
-    while ('0' <= *str && *str <= '9')
-    {
-        res = res * 10 + *str - '0';
-        ++str;
-    }
-
-    assert(str != str_old);
-
-    NUM_CTOR(value, res);
-
-    return str;
-}
-
-const char *GetVariable(const char *str, Node *value)
-{
-    const char *str_old = str;
-
-    while (isalpha(*str))
-        ++str;
-
-    assert(str != str_old);
-
-    *value = 
-        {
-            .type  = TYPE_VAR,
-            .value = {.var = strndup(str_old, str - str_old)},
-            .left  = NULL,
-            .right = NULL
-        };
-
-    return str;
-}
-
-const char *GetFunction(const char *str, Node *value)
-{
-    int32_t op = 0;
-
-    if (strncmp(str, "sin", 3) == 0)
-    {
-        op = OP_SIN;
-        str += 3;
-    }
-    else if (strncmp(str, "cos", 3) == 0)
-    {
-        op = OP_COS;
-        str += 3;
-    }
-    else if (strncmp(str, "ln", 2) == 0)
-    {
-        op = OP_LN;
-        str += 2;
-    }
-    else
-        assert(0 && "Wrong function");
-
-    assert(*str == '(');
-    ++str;
-
-    Node *arg = NodeNew();
-    str = GetExpression(str, arg);
-
-    assert(*str == ')');
-    ++str;
-
-    OP_CTOR(value, op, NULL, arg);
-
-    return str;
-}
-
-#define CURR node
 
 void Simplify(Node *node)
 {
--- diffr.h
+++ diffr.h
@@ -4,50 +4,7 @@
 #include "iostr.h"
 #include "tree.h"
 #include "stack.h"
-
-#define NUM_CTOR(node, val)   NodeCtor   (node,     TYPE_NUM, {.dbl = val}, NULL, NULL)
-#define CREATE_NUM(val)       CreateNode (          TYPE_NUM, {.dbl = val}, NULL, NULL)
-#define OP_CTOR(node, op, lhs, rhs) NodeCtor (node, TYPE_OP, {.op = op}, lhs, rhs)
-
-#define LEFT           (CURR)->left
-#define RIGHT          (CURR)->right
-
-#define D_L            Differentiate (LEFT)
-#define D_R            Differentiate (RIGHT)
-
-#define CP_L           TreeCopy (LEFT)
-#define CP_R           TreeCopy (RIGHT)
-#define CP_CR          TreeCopy (CURR)
-
-#define ADD(lhs, rhs)  CreateNode (TYPE_OP, {.op = OP_ADD}, lhs,          rhs)
-#define SUB(lhs, rhs)  CreateNode (TYPE_OP, {.op = OP_SUB}, lhs,          rhs)
-
-#define MUL(lhs, rhs)  CreateNode (TYPE_OP, {.op = OP_MUL}, lhs,          rhs)
-#define DIV(lhs, rhs)  CreateNode (TYPE_OP, {.op = OP_DIV}, lhs,          rhs)
-
-#define SIN(rhs)       CreateNode (TYPE_OP, {.op = OP_SIN}, NULL, rhs)
-#define COS(rhs)       CreateNode (TYPE_OP, {.op = OP_COS}, NULL, rhs)
-
-#define EXP(lhs, rhs)  CreateNode (TYPE_OP, {.op = OP_EXP}, lhs,           rhs)
-#define LN(rhs)        CreateNode (TYPE_OP, {.op = OP_LN},  NULL, rhs)
-
-#define IS_OP(node)               (GET_TYPE(node) == TYPE_OP)
-#define IS_OP_CODE(node, op_code) (IS_OP(node) && GET_OP(node) == op_code)
-
-#define IS_NUM(node)              (node != NULL && GET_TYPE(node) == TYPE_NUM)
-
-#define EPS                        1e-6
-#define IS_EQ(node, val)          (IS_NUM(node) && (val) - EPS < GET_NUM(node) && GET_NUM(node) < (val) + EPS)
-#define IS_ZERO(node)             (IS_EQ(node, 0))
-#define IS_ONE(node)              (IS_EQ(node, 1))
-
-#define IS_VAR(node)              (node != NULL && GET_TYPE(node) == TYPE_VAR)
-
-#define IS_FUNC(node)             (node != NULL && (IS_OP(node) || IS_VAR(node)))
-
-#define GET_TYPE(node)            ((node)->type)
-#define GET_NUM(node)             ((node)->value.dbl)
-#define GET_OP(node)              ((node)->value.op)
+#include "dsl.h"
 
 enum OPTIONS
 {
@@ -108,14 +65,5 @@ void        RotateCommutative  (Node *node);
 Node       *CreateNode         (int32_t type, NodeValue val, Node *left, Node *right);
 
 const char *GetOperatorString  (int32_t op_code);
-
-const char *GetGeneral    (const char *str, Node *value);
-const char *GetExpression (const char *str, Node *value);
-const char *GetProduct    (const char *str, Node *value);
-const char *GetPower      (const char *str, Node *value);
-const char *GetPrimary    (const char *str, Node *value);
-const char *GetNumber     (const char *str, Node *value);
-const char *GetVariable   (const char *str, Node *value);
-const char *GetFunction   (const char *str, Node *value);
 
 #endif  // DIFFR_H
